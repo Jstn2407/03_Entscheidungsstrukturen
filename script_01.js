d974839cbefd6d7457856ccec0466023b034336a
Justin <justinpessel@gmail.com>
2021-02-09 11:45:11 +0100

    01_IF

--- script_01.js
+++ script_01.js
@@ -5,5 +5,24 @@ let ageJohn, ageMark;
 let isJohnOlder, isJohnEqual;
 
 // Wertzuweisung
-ageJohn = 35;
+ageJohn = 55;
 ageMark = 30;
+
+//Logische Aussagen/ Test(s)
+isJohnOlder = (ageJohn >ageMark); // 30 > 30
+isJohnEqual = (ageJohn == ageMark); // 30 = 30
+
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("--------------------");
+
+/************IF *******************/
+
+//if (true)
+if (isJohnOlder)
+//if (ageJohn > ageMark)
+//if (55 > 30) 
+{
+    console.log("John ist älter!");
+}
+
